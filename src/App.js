31d1b042f78306b346d47a5ada388044e7aaab83
황윤진 <dbswlsghkd@gmail.com>
2024-09-20 20:38:20 +0900

    컴포넌트 만들기2, 화살표 함수로 변경

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import './App.css';
 
-function App() {
+const App = () => {
     return (
         <div className="App">
             <Subject></Subject>
+            <TOC></TOC>
+            <Content></Content>
         </div>
     );
-}
+};
+
+const Content = () => {
+    return (
+        <article>
+            <h2>HTML</h2>
+            HTML is HyperText Markup Language.
+        </article>
+    );
+};
+
+const TOC = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="1.html">HTML</a>
+                </li>
+                <li>
+                    <a href="2.html">CSS</a>
+                </li>
+                <li>
+                    <a href="3.html">JavaScript</a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
 
 // 하나의 최상위 태그만 사용해야함
 function Subject() {
