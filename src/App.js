f07ba05520fa653764c87829f7b1b612e7d31b6b
황윤진 <dbswlsghkd@gmail.com>
2024-09-21 11:47:35 +0900

    컴포넌트, props 이해

--- src/App.js
+++ src/App.js
@@ -1,50 +1,57 @@
 import './App.css';
 
-const App = () => {
-    return (
-        <div className="App">
-            <Subject></Subject>
-            <TOC></TOC>
-            <Content></Content>
-        </div>
-    );
-};
-
-const Content = () => {
-    return (
-        <article>
-            <h2>HTML</h2>
-            HTML is HyperText Markup Language.
-        </article>
-    );
-};
-
-const TOC = () => {
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <a href="1.html">HTML</a>
-                </li>
-                <li>
-                    <a href="2.html">CSS</a>
-                </li>
-                <li>
-                    <a href="3.html">JavaScript</a>
-                </li>
-            </ul>
-        </nav>
-    );
-};
-
 // 하나의 최상위 태그만 사용해야함
-function Subject() {
+function Headder(props) {
+    console.log(props);
     return (
         <header>
-            <h1>WEB</h1>
+            <h1>{props.title}</h1>
             world wide web!
         </header>
     );
 }
+
+const Nav = (props) => {
+    const lis = [];
+
+    for (let i = 0; i < props.topics.length; i++) {
+        let t = props.topics[i];
+        lis.push(
+            <li key={t.id}>
+                <a href={'/read/' + t.id}>{t.title}</a>
+            </li>
+        );
+    }
+    return (
+        <nav>
+            <ol>{lis}</ol>
+        </nav>
+    );
+};
+
+const Article = (props) => {
+    return (
+        <article>
+            <h2>{props.title}</h2>
+            {props.body}
+        </article>
+    );
+};
+
+const App = () => {
+    const topics = [
+        { id: 1, title: 'html', body: 'html is ...' },
+        { id: 2, title: 'css', body: 'css is ...' },
+        { id: 3, title: 'javascript', body: 'javascript is ...' },
+    ];
+    return (
+        <div>
+            <Headder title="REACT"></Headder>
+            <Nav topics={topics}></Nav>
+            <Article title="Welcome" body="Hello, WEB"></Article>
+            <Article title="Hi" body="Hello, REACT"></Article>
+        </div>
+    );
+};
 
 export default App;
