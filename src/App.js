9d4949c256200124dc7d9b1cf1dc1e1329d6720b
황윤진 <dbswlsghkd@gmail.com>
2024-09-21 14:50:33 +0900

    create에 대한 이해

--- src/App.js
+++ src/App.js
@@ -57,6 +57,34 @@ const Article = (props) => {
     );
 };
 
+const Create = (props) => {
+    return (
+        <article>
+            <h2>Create</h2>
+            <form
+                onSubmit={(event) => {
+                    event.preventDefault();
+                    const title = event.target.title.value;
+                    const body = event.target.body.value;
+                    console.log(title, 'title');
+                    console.log(body, 'body');
+                    props.onCreate(title, body);
+                }}
+            >
+                <p>
+                    <input type="text" name="title" placeholder="title"></input>
+                </p>
+                <p>
+                    <textarea name="body" placeholder="body"></textarea>
+                </p>
+                <p>
+                    <input type="submit" value="Create"></input>
+                </p>
+            </form>
+        </article>
+    );
+};
+
 const App = () => {
     // const _mode = useState('WELCOME');
     // console.log(_mode, '_mode');
@@ -65,12 +93,13 @@ const App = () => {
 
     const [mode, setMode] = useState('WELCOME');
     const [id, setId] = useState(null);
+    const [nextId, setNextId] = useState(4);
 
-    const topics = [
+    const [topics, setTopics] = useState([
         { id: 1, title: 'html', body: 'html is ...' },
         { id: 2, title: 'css', body: 'css is ...' },
         { id: 3, title: 'javascript', body: 'javascript is ...' },
-    ];
+    ]);
     let content = null;
     if (mode === 'WELCOME') {
         content = <Article title="Welcome" body="Hello, WEB"></Article>;
@@ -84,6 +113,23 @@ const App = () => {
             }
         }
         content = <Article title={title} body={body}></Article>;
+    } else if (mode === 'CREATE') {
+        content = (
+            <Create
+                onCreate={(_title, _body) => {
+                    const newTopic = { id: nextId, title: _title, body: _body };
+                    // topics의 복제본
+                    const newTopics = [...topics];
+                    // 복제본에 데이터 추가
+                    newTopics.push(newTopic);
+                    // 복제본을 비교해서 다르면 랜더링 시켜줌
+                    setTopics(newTopics);
+                    setMode('READ');
+                    setId(nextId);
+                    setNextId(nextId + 1);
+                }}
+            ></Create>
+        );
     }
     return (
         <div>
@@ -101,6 +147,15 @@ const App = () => {
                 }}
             ></Nav>
             {content}
+            <a
+                herf="/create"
+                onClick={(event) => {
+                    event.preventDefault();
+                    setMode('CREATE');
+                }}
+            >
+                Create
+            </a>
         </div>
     );
 };
